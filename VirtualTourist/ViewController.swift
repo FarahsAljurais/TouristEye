5c9ada698915d87863e3c9fe950a65cca661a242
Farah <FarahAljurais@gmail.com>
2020-08-18 03:02:54 +0300

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Farah Aljurais on 28/12/1441 AH.
+//  Copyright © 1441 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
